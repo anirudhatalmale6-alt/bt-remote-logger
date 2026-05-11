cb0ad197d95f2067eccad74e090fbcf655eddb17
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-05-11 18:15:51 +0000

    Fix iOS black screen: use object_setClass instead of window replacement
    Creating a new EventWindow and moving rootViewController triggers
    'Manually adding rootViewController view to hierarchy' error on
    modern iOS. Instead, use object_setClass to change the existing
    window's class to EventWindow in-place. Same window object, same
    view hierarchy, but now has EventWindow's method overrides.

--- ios/BtRemoteLogger/AppDelegate.mm
+++ ios/BtRemoteLogger/AppDelegate.mm
@@ -2,6 +2,7 @@
 #import "EventWindow.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <objc/runtime.h>
 
 @implementation AppDelegate
 
@@ -12,22 +13,8 @@
 
   BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
 
-  UIWindow *oldWindow = self.window;
-  if (oldWindow && ![oldWindow isKindOfClass:[EventWindow class]]) {
-    EventWindow *ew = nil;
-    if (@available(iOS 13.0, *)) {
-      UIWindowScene *scene = oldWindow.windowScene;
-      if (scene) {
-        ew = [[EventWindow alloc] initWithWindowScene:scene];
-      }
-    }
-    if (!ew) {
-      ew = [[EventWindow alloc] initWithFrame:oldWindow.frame];
-    }
-    ew.rootViewController = oldWindow.rootViewController;
-    ew.backgroundColor = oldWindow.backgroundColor;
-    self.window = ew;
-    [ew makeKeyAndVisible];
+  if (self.window && ![self.window isKindOfClass:[EventWindow class]]) {
+    object_setClass(self.window, [EventWindow class]);
   }
 
   return result;
